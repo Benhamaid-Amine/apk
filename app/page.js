24699a61e74cbefd54c7bce297e60a7554178726
Benhamaid-Amine <aminocheminou@gmail.com>
2025-04-01 13:32:31 +0100

    apk1.4

--- app/page.js
+++ app/page.js
@@ -2,14 +2,14 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
       <h1 className="text-3xl text-white  font-bold mb-6">Bienvenue sur mon site</h1>
-      <p className="text-lg text-white mb-4">Cliquez ci-dessous pour télécharger l'application :</p>
+      <p className="text-lg text-white mb-4">Cliquez ci-dessous pour télécharger lapplication :</p>
       
       <a
         href="/app.apk"
         download
         className="px-6 py-3 bg-blue-500 text-white text-lg rounded-lg shadow-md hover:bg-blue-600 transition"
       >
-        📥 Télécharger l'APK
+        📥 Télécharger l APK
       </a>
       <h1>hello world !!</h1>
     </div>
